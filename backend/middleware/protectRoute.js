173625a561004b8180866744123a1f07904c8b3c
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-05 23:10:07 +0530

    Add User Routes && Controllers

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -5,8 +5,9 @@ export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      return res.status(401).json({ error: "you need to login first" });
+      return res.status(401).json({ error: "Unauthorized: No Token Provided" });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
@@ -18,11 +19,11 @@ export const protectRoute = async (req, res, next) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
+
     req.user = user;
     next();
   } catch (err) {
     console.log("Error in protectRoute middleware", err.message);
-
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded);
 app.use(cookieParser());
 console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
