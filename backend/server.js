130e1435c681772389fb931c93449afd245841c6
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-07-07 20:34:36 +0530

    Add Notification Routes && Controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -24,6 +25,7 @@ console.log(process.env.MONGO_URI);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notification", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port  ${PORT}`);
